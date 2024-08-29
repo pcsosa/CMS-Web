ea2f0392c6b8cc5d8fd9c3761a5bca23ab6875cd
Ezequiel Aguilera <ezequielagui102@gmail.com>
2024-08-29 02:56:48 -0400

    Corregir el login de google y cambiar nombres de variables de entorno

--- appcms/urls.py
+++ appcms/urls.py
@@ -1,7 +1,9 @@
 # myapp/urls.py (Archivo de la aplicación 'myapp')
 from django.urls import path
 from . import views
+from .views import Home
 
 urlpatterns = [
     # Otras rutas de la aplicación
+    path("", Home.as_view(), name="home")
 ]
