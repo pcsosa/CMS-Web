08d11ee54cd84f87a65306dae704d1c4aef49419
mabejoyb <mabejoyb@gmail.com>
2024-08-29 09:48:10 -0400

    Creacion de Categoria en model y en view

--- appcms/views.py
+++ appcms/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from .models import Categoria
 from django.views.generic import TemplateView
+
 # Create your views here.
 
 class Home(TemplateView):
     template_name = "home.html"
+
+
+def buscar_categorias(request):
+    consulta = request.GET.get('q')
+    categorias = []
+    mensaje = ''
+    
+    if consulta:
+        categorias = Categoria.objects.filter(nombre__icono_contiene = consulta)
+        if not categorias.exists():
+            mensaje = "Categoría no encontrada"
+    
+    contexto = {
+        'categorias': categorias,
+        'mensaje': mensaje,
+        'consulta': consulta
+    }
+    
+    return render(request, 'buscar_categorias.html', contexto)
