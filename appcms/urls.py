ae7248418793022e701f28d321e23717ca5d1368
Ezequiel Aguilera <ezequielagui102@gmail.com>
2024-08-29 19:16:36 -0400

    Arreglar login

--- appcms/urls.py
+++ appcms/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path
 from .views import buscar_categorias
 from . import views
-#from .views import Home
+from .views import Home
 
 urlpatterns = [
+    path('', Home.as_view(), name='home'),
     path('search/', buscar_categorias, name='buscar_categorias'),
 ]
