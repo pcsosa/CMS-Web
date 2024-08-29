63581cf0d4fe71c00ec2a4181fc177ecbe04f157
mabejoyb <mabejoyb@gmail.com>
2024-08-29 09:48:10 -0400

    Creacion de Categoria en model y en view

--- appcms/urls.py
+++ appcms/urls.py
@@ -1,9 +1,14 @@
 # myapp/urls.py (Archivo de la aplicación 'myapp')
 from django.urls import path
+from .views import search_categories
 from . import views
 from .views import Home
 
 urlpatterns = [
     # Otras rutas de la aplicación
     path("", Home.as_view(), name="home")
+]
+
+urlpatterns = [
+    path('search/', search_categories, name='search_categories'),
 ]
