51d00d8be733f525297d383abad3b2e9cdbce7a9
satf029 <torressamuel.ferna@gmail.com>
2024-08-30 02:41:32 -0400

    crear subcategoria

--- appcms/urls.py
+++ appcms/urls.py
@@ -1,6 +1,6 @@
 # myapp/urls.py (Archivo de la aplicación 'myapp')
 from django.urls import path
-from .views import buscar_categorias, crear_categoria, lista_categorias
+from .views import buscar_categorias, crear_categoria, lista_categorias, crear_subcategorias
 from . import views
 from .views import Home
 
@@ -9,6 +9,8 @@ urlpatterns = [
     path('search/', buscar_categorias, name='buscar_categorias'),
     path('admincat/', views.administrar_categorias, name='administrar_categorias'),
     path('lista/', views.lista_categorias, name='lista_categorias'),
-      path('crear/', views.crear_categoria, name='crear_categoria'),
+    path('crear/', views.crear_categoria, name='crear_categoria'),
     path('lista/', views.lista_categorias, name='lista_categorias'),
+    path('crear_subcategorias/', views.crear_subcategorias, name='crear_subcategorias'),
+    path('listar_sub/', views.lista_subcategorias, name='lista_subcategorias'),
 ]
