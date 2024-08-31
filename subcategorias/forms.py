97b2e9028fece010f33929baf2509b80c441f748
satf029 <torressamuel.ferna@gmail.com>
2024-08-31 18:06:41 -0400

    docstring de forms de subcategoria

--- subcategorias/forms.py
+++ subcategorias/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from .models import Subcategoria
 class SubcategoriaForm(forms.ModelForm):
+    """
+    Formulario para crear una subcategoria nueva.
+    
+    :meta: Forma relacionada con el modelo UserProfile.
+    :fields: Los campos que se incluirán en el formulario.
+    """
     class Meta:
         model = Subcategoria
         fields = ['nombre','categoria']
