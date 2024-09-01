7a659327f0af4ae6bf125246fa462cb1df23354f
satf029 <torressamuel.ferna@gmail.com>
2024-09-01 12:44:38 -0400

    documentacion con sphinx subcategorias-modelos,forms y views

--- docs/conf.py
+++ docs/conf.py
@@ -7,18 +7,25 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0,os.path.abspath(".."))
+import django
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cmsweb.settings'
+django.setup()
 
-
-project = 'CMSWEB  Documentacion'
-copyright = '2024, Equipo01'
-author = 'Equipo01'
-release = '1.0.0'
+project = 'cmsweb'
+copyright = '2024, equipo01'
+author = 'equipo01'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc"]
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary', 
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -28,5 +35,5 @@ language = 'es'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_theme = 'classic'
+html_static_path = ['_static']
