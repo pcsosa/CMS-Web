7e02fc3c045c83bf3607139cb02fea782af0daec
Ezequiel Aguilera <ezequielagui102@gmail.com>
2024-09-01 17:50:34 -0400

    tests inicial

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -4,7 +4,13 @@ from appcms.views import buscar_categorias
 
 
 class TestUrls(SimpleTestCase):
+  """
+  Clase para testear todas las urls
+  """
 
-    def test_list_url_is_resolved(self):
-        url = reverse( 'buscar_categorias')
-        self.assertEqual(resolve(url).func, buscar_categorias)
+  def test_list_url_is_resolved(self):
+    """
+    Función para testear todas las urls
+    """
+    url = reverse( 'buscar_categorias')
+    self.assertEqual(resolve(url).func, buscar_categorias)
