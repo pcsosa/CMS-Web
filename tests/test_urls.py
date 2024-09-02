109c2257aee93b0ce548b4dc129b123fc535fe33
pcsosa <pcsosa.pcs@fpuna.edu.py>
2024-09-01 23:04:59 -0400

    prueba unitaria views

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -9,22 +9,22 @@ class TestUrls(SimpleTestCase):
   Clase para testear todas las urls
   """
 
-    def test_buscar_categorias_url_resolves(self):
-        url = reverse( 'buscar_categorias')
-        self.assertEqual(resolve(url).func, buscar_categorias)
+def test_buscar_categorias_url_resolves(self):
+    url = reverse( 'buscar_categorias')
+    self.assertEqual(resolve(url).func, buscar_categorias)
 
-    def test_administrar_categorias_url_resolves(self):
-        url = reverse( 'administrar_categorias')
-        self.assertEqual(resolve(url).func, administrar_categorias)
+def test_administrar_categorias_url_resolves(self):
+    url = reverse( 'administrar_categorias')
+    self.assertEqual(resolve(url).func, administrar_categorias)
 
-    def test_lista_categorias_url_resolves(self):
-        url = reverse( 'lista_categorias')
-        self.assertEqual(resolve(url).func, lista_categorias)
+def test_lista_categorias_url_resolves(self):
+    url = reverse( 'lista_categorias')
+    self.assertEqual(resolve(url).func, lista_categorias)
 
-    def test_login_url_is_resolved(self): 
-        url = reverse('login')
-        resolved_view = resolve(url).func.view_class 
-        self.assertEqual(resolved_view, auth_views.LoginView)
+def test_login_url_is_resolved(self): 
+    url = reverse('login')
+    resolved_view = resolve(url).func.view_class 
+    self.assertEqual(resolved_view, auth_views.LoginView)
 
 
             
