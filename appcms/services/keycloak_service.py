d67ddd550fe2df6bf120b04a2084aa294944f8d5
pcsosa <pcsosa.pcs@fpuna.edu.py>
2024-09-06 13:03:48 -0400

    documentación y permisos varios

--- appcms/services/keycloak_service.py
+++ appcms/services/keycloak_service.py
@@ -6,8 +6,33 @@ import os
 load_dotenv()
 
 class KeycloakService:
+  """
+  Clase que proporciona servicios de autenticación e interacción con Keycloak.
 
+  Esta clase encapsula las operaciones relacionadas con la autenticación de usuarios y la administración 
+  utilizando el cliente KeycloakOpenID y KeycloakAdmin.
+
+  :param server_url: La URL del servidor Keycloak.
+  :type server_url: str
+  :param client_id: El ID del cliente registrado en Keycloak.
+  :type client_id: str
+  :param realm_name: El nombre del reino en Keycloak.
+  :type realm_name: str
+  :param client_secret_key: La clave secreta del cliente para autenticación.
+  :type client_secret_key: str
+  :param username: Nombre de usuario para la administración de Keycloak.
+  :type username: str
+  :param password: Contraseña para la administración de Keycloak.
+  :type password: str
+  :param user_realm_name: Nombre del reino del usuario para la administración.
+  :type user_realm_name: str
+  """
   def __init__(self):
+    """
+      Inicializa una instancia de KeycloakService con las configuraciones necesarias.
+
+      Configura los clientes KeycloakOpenID y KeycloakAdmin usando las configuraciones del entorno.
+    """
     self.openid = KeycloakOpenID(
       server_url=settings.KEYCLOAK_SERVER_URL,
       client_id=settings.KEYCLOAK_CLIENT_ID,
@@ -23,6 +48,16 @@ class KeycloakService:
     )
 
   def get_token(self, code):
+    """
+    Obtiene un token de acceso de Keycloak usando un código de autorización.
+
+    Utiliza el código de autorización recibido para obtener un token de acceso y de actualización.
+
+    :param code: El código de autorización recibido de Keycloak.
+    :type code: str
+    :return: Un diccionario con el token de acceso y otros datos relacionados.
+    :rtype: dict
+    """
     redirect_uri = os.getenv('DJ_URL') + ':' + os.getenv('DJ_PORT') + '/callback/'
     token = self.openid.token(
         code = code,
@@ -32,11 +67,41 @@ class KeycloakService:
     return token
   
   def get_userId(self, token):
+    """
+      Obtiene el ID del usuario a partir del token de acceso.
+
+      Extrae el ID del usuario (sub) del token de acceso proporcionado.
+
+      :param token: El token de acceso de Keycloak.
+      :type token: dict
+      :return: El ID del usuario.
+      :rtype: str
+      """  
     user_info = self.openid.userinfo(token['access_token'])
     return user_info.get('sub')
   
   def isActive(self, token):
+    """
+    Verifica si el token de acceso está activo.
+
+    Realiza una introspección del token para determinar su estado de actividad.
+
+    :param token: El token de acceso de Keycloak.
+    :type token: dict
+    :return: Verdadero si el token está activo, falso en caso contrario.
+    :rtype: bool
+    """
     return self.openid.introspect(token).get('active')
     
   def renovarToken(self, token):
+    """
+    Renueva el token de acceso usando el token de actualización.
+
+    Solicita un nuevo token de acceso utilizando el token de actualización proporcionado.
+
+    :param token: El token de actualización de Keycloak.
+    :type token: dict
+    :return: Un diccionario con el nuevo token de acceso y otros datos relacionados.
+    :rtype: dict
+    """
     return self.openid.refresh_token(token['refresh_token'])
