94e0a44b9ab5f611e2eb09854e7e40712bc336e9
mabejoyb <mabejoy@fpuna.edu.py>
2024-09-15 21:51:36 -0400

    Documentacion en archivos varios.

--- appcms/context_processors.py
+++ appcms/context_processors.py
@@ -5,6 +5,13 @@ from appcms.utils.utils import obtener_roles_desde_token
 def datos_basicos(request):
     start_time = time.time()
     
+    """
+    Obtiene la información básica del usuario autenticado y sus roles desde Keycloak.
+
+    :param request: Objeto HttpRequest que contiene los datos de la sesión, incluyendo el token.
+    :return: Un diccionario con la información del usuario y sus roles si el token está presente, o un diccionario vacío si no hay token.
+    :rtype: dict
+    """
     kc = KeycloakService.get_instance()
     token = request.session.get('token')
     
