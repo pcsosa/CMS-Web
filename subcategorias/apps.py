ae9827a927f77eb3baec52d1681a326697c10ae4
mabejoyb <mabejoy@fpuna.edu.py>
2024-09-15 21:51:36 -0400

    Documentacion en archivos varios.

--- subcategorias/apps.py
+++ subcategorias/apps.py
@@ -3,15 +3,10 @@ from django.apps import AppConfig
 
 class SubcategoriasConfig(AppConfig):
     """
-    Configuración de la aplicación 'subcategorias'.
+    Configuración de la aplicación Subcategorias.
 
-    Esta clase configura la aplicación Django para manejar subcategorías dentro del sistema.
-
-    :param default_auto_field: Tipo de campo utilizado para la clave primaria automática.
-    :type default_auto_field: str
-
+    :param default_auto_field: Tipo de campo por defecto para las claves primarias.
     :param name: Nombre de la aplicación dentro del proyecto Django.
-    :type name: str
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'subcategorias'
