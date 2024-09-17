6ecfcd9483f24db01772de6b67d82c496265c9c2
hezqiel7 <ezequielagui102@gmail.com>
2024-09-17 04:11:00 -0400

    formatear documentos

--- appcms/context_processors.py
+++ appcms/context_processors.py
@@ -1,17 +1,16 @@
-from appcms.utils.utils import obtenerToken, obtenerUserInfo, obtenerRolesUser
+from appcms.utils.utils import obtenerRolesUser, obtenerToken, obtenerUserInfo
+
 
 def datos_basicos(request):
+
     token = obtenerToken(request)
-    
+
     if not token:
         return {}
-    
+
     user_info = obtenerUserInfo(token)
     roles = obtenerRolesUser(token)
 
-    contexto = {
-      'user_info': user_info,
-      'roles': roles
-    }
+    contexto = {"user_info": user_info, "roles": roles}
 
     return contexto
--- appcms/decorators.py
+++ appcms/decorators.py
@@ -1,13 +1,16 @@
 from functools import wraps
+
 from django.http import HttpResponseForbidden
-from appcms.utils.utils import obtenerRolesUser, obtenerToken, comprobarToken
+
+from appcms.utils.utils import comprobarToken, obtenerRolesUser, obtenerToken
+
 
 def roles_requeridos(*required_roles):
     """
     Decorador para restringir el acceso a vistas basado en los roles del usuario.
 
-    Este decorador verifica si el usuario tiene uno de los roles requeridos para acceder a una vista. 
-    Si el usuario tiene el rol adecuado, la vista se ejecuta; de lo contrario, se devuelve una respuesta de 
+    Este decorador verifica si el usuario tiene uno de los roles requeridos para acceder a una vista.
+    Si el usuario tiene el rol adecuado, la vista se ejecuta; de lo contrario, se devuelve una respuesta de
     prohibición de acceso.
 
     :param required_roles: Roles requeridos para acceder a la vista. Puede incluir uno o más roles.
@@ -15,6 +18,7 @@ def roles_requeridos(*required_roles):
     :return: Una función decoradora que envuelve la vista objetivo.
     :rtype: function
     """
+
     def decorator(view_func):
         """
         Función interna que envuelve la vista objetivo y aplica la lógica de autorización.
@@ -24,34 +28,38 @@ def roles_requeridos(*required_roles):
         :return: La vista decorada con la lógica de autorización aplicada.
         :rtype: function
         """
+
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
             """
             Verifica los roles del usuario y decide si permitir el acceso a la vista.
 
-            Obtiene el token de sesión, comprueba su validez y verifica los roles del usuario. 
-            Si el usuario tiene uno de los roles requeridos, ejecuta la vista. De lo contrario, 
+            Obtiene el token de sesión, comprueba su validez y verifica los roles del usuario.
+            Si el usuario tiene uno de los roles requeridos, ejecuta la vista. De lo contrario,
             devuelve una respuesta de prohibición de acceso.
 
             :param request: El objeto de solicitud HTTP.
             :type request: django.http.HttpRequest
             :param args: Argumentos adicionales para la vista.
             :param kwargs: Argumentos de palabra clave adicionales para la vista.
-            :return: La respuesta de la vista si el usuario tiene los roles adecuados, 
+            :return: La respuesta de la vista si el usuario tiene los roles adecuados,
                      o una respuesta de prohibición de acceso si no los tiene.
             :rtype: django.http.HttpResponse
             """
-            
+
             token = obtenerToken(request)
 
             token = comprobarToken(request, token)
-            
+
             if not token:
-              return HttpResponseForbidden("No tienes permiso para acceder a esta página.")
-            
+                return HttpResponseForbidden(
+                    "No tienes permiso para acceder a esta página."
+                )
+
             user_roles = obtenerRolesUser(token)
             if any(element in user_roles for element in required_roles):
                 return view_func(request, *args, **kwargs)
-            
+
         return _wrapped_view
+
     return decorator
