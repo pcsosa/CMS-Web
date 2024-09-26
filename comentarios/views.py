2b2d5bfd985496c46c152264e4ee2d946dc6e360
edisrocio <adis8@fpuna.edu.py>
2024-09-26 00:52:35 -0400

    Creando views y html de comentarios

--- comentarios/views.py
+++ comentarios/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
+
+def post_detail(request, slug):
+    template = 'commentarios.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, template_name, {'post': post, 'comments': comments,'new_comment': new_comment, 'comment_form': comment_form})
