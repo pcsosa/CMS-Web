ea9dc115cd5eb666e438398fe434ac3804f1448b
edisrocio <adis8@fpuna.edu.py>
2024-09-27 09:12:23 -0400

    errores en bd

--- cmsweb/settings.py
+++ cmsweb/settings.py
@@ -66,17 +66,16 @@ ALLOWED_HOSTS = [
 # -------------------------------------------------
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'appcms',
-    'contenidos',
-    'subcategorias',
-    
-]
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "appcms",
+    "contenidos",
+    "subcategorias",
+] #
 
 # -------------------------------------------------
 # ----------------- MIDDLEWARES -------------------
