2b876efb9da66d5baa00dd55bd3c37c9b8a1abfe
satf029 <torressamuel.ferna@gmail.com>
2024-09-27 14:18:11 -0400

    agregacion de enlaces y cambios de estados en kanbam

--- contenidos/views_cont.py
+++ contenidos/views_cont.py
@@ -327,8 +327,7 @@ def visualizar_contenido(request, pk):
     except Contenido.DoesNotExist:
         return JsonResponse({'error': 'Contenido no encontrado'}, status=404)
     
-def cambiar_estado(request,pk,estado_actual,estado_siguiente):
-    
+def cambiar_estado(request, pk, estado_actual, estado_siguiente):
     contenido = get_object_or_404(Contenido, pk=pk)
     estados_disponibles = ('Borrador', 'Revisión', 'A Publicar', 'Publicado', 'Inactivo')
 
@@ -338,17 +337,26 @@ def cambiar_estado(request,pk,estado_actual,estado_siguiente):
         siguiente = estados_disponibles.index(estado_siguiente)
 
         # Si el estado siguiente no es 'Inactivo' y el estado actual no es 'Publicado'
-        if (estado_siguiente != 'Inactivo'):
-            
+        if estado_siguiente != 'Inactivo':
             # Verifica que los estados estén uno al lado del otro
-            if abs(actual - siguiente) == 1 or estado_actual=='Inactivo':
+            if abs(actual - siguiente) == 1 or estado_actual == 'Inactivo':
                 contenido.estado = estado_siguiente
                 contenido.save()
-                return redirect('visualizar_contenido', pk)
-            else:
-                messages.error(request, "No se pudo cambiar de estado")
-                return redirect(request.META.get('HTTP_REFERER'))
+                #messages.success(request, "El estado ha sido cambiado exitosamente.")
+            #else:
+                #messages.error(request, "No se pudo cambiar de estado.")
         else:
             contenido.estado = estado_siguiente
             contenido.save()
-            return redirect('visualizar_contenido', pk)
+            #messages.success(request, "El contenido ha sido inactivado.")
+    #else:
+        #messages.error(request, "Estados inválidos proporcionados.")
+
+    # Manejar la redirección si HTTP_REFERER no está presente
+    referer = request.META.get('HTTP_REFERER')
+
+    if referer:
+        return redirect(referer)
+    else:
+        # Si no hay referer, redirigir a una vista por defecto
+        return redirect('tablero_kanban')
