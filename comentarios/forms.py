ba5ca9f8269de87e024a6054dc41dc51506e599c
edisrocio <adis8@fpuna.edu.py>
2024-09-26 23:43:01 -0400

    cambios prueba

--- comentarios/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import Comentario
-from django import formsclass
-
- CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comentario
-        fields = ('usuario', 'email', 'comentario')
