3519e3994344069a8a458dc2a86e75c714b9e653
hezqiel7 <ezequielagui102@gmail.com>
2024-09-29 18:21:05 -0400

    arreglar modal y ocultar botones

---
+++ static/js/lista_categorias.js
@@ -0,0 +1,30 @@
+function redirigirUrl(categoriaId) {
+  // Obtener la URL actual
+  let currentUrl = window.location.href
+
+  // Reemplazar '/lista/' con '/adminsub/lista/' y añadir el ID de la categoría
+  let newUrl = currentUrl.replace('/cat/', `/adminsub/lista/${categoriaId}`)
+
+  // Redirigir a la nueva URL
+  window.location.href = newUrl
+}
+
+$('#crearModal').on('hidden.bs.modal', function () {
+  $(this).find('form').trigger('reset')
+})
+
+$('#editModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Botón que abrió el modal
+  var id = button.data('id') // Extrae el ID de los datos del botón
+  var nombre = button.data('nombre') // Extrae el nombre de los datos del botón
+  var descripcion = button.data('descripcion') // Extrae la descripción de los datos del botón
+
+  var modal = $(this)
+  modal.find('#categoriaId').val(id)
+  modal.find('#nombre').val(nombre)
+  modal.find('#descripcion').val(descripcion)
+
+  // Actualiza la URL del formulario en el modal
+  var actionUrl = "{% url 'editar_categoria' 0 %}".replace('0', id)
+  modal.find('form').attr('action', actionUrl)
+})
