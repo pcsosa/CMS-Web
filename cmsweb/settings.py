9f773cd2c3415164a010e0ab9e7d0dc293b243a6
satf029 <torressamuel.ferna@gmail.com>
2024-10-18 16:22:13 -0300

    ocultamiento de datos

--- cmsweb/settings.py
+++ cmsweb/settings.py
@@ -210,10 +210,9 @@ USE_I18N = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Configuración para enviar correos electrónicos a través de Gmail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cmsequipo1@gmail.com'  
-#EMAIL_HOST_PASSWORD = 'E1P2J3E4s5'
-EMAIL_HOST_PASSWORD = 'aemmajvqqsawyedn'
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
